008b59d1ce7743de31be77c2dd6413a2cb33877e
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2022-10-20 18:46:40 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Corecalc
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Workbook wb = new Workbook();
+            Sheet sheet = new Sheet(wb, "Sheet 1", false);
+            sheet.SetCell(new TextCell("abc"), 0,0);
+            sheet.SetCell(new NumberCell(33.33), 0,1);
+            wb.AddSheet(sheet);
+            var x = 0;
+            foreach(var s in wb) {
+                var y = 0;
+                foreach (var c in s)
+                {
+                    Console.WriteLine(c.ShowValue(sheet,x,y));
+                    y++;
+                }
+                x++;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Corecalc and Funcalc
+
+Adapted for .net core to run on linux.
+
+See [Corecalc and Funcalc website for information](https://www.itu.dk/people/sestoft/funcalc/) on the original project.
+
+## Authors and credits
+
+The implementation is Copyright 2006-2014 Peter Sestoft and others, and is released under a [MIT-style open license](https://www.itu.dk/people/sestoft/funcalc/LICENSE.TXT).
