5be410619885b656be3ba903b069688981b6dd97
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2022-10-21 22:34:15 +0200

    Use File-Scoped Namespaces

--- Funcalc/Variable.cs
+++ Funcalc/Variable.cs
@@ -28,121 +28,119 @@ using System.Collections.Generic;
 using System.Reflection.Emit;
 using System.Text;
 
-namespace Corecalc.Funcalc
+namespace Corecalc.Funcalc;
+/// <summary>
+/// A Variable represents a cell of a sheet-defined function, whether
+/// an argument (input cell) or a computed cell (intermediate or output cell).
+/// </summary>
+public abstract class Variable : IEquatable<Variable>
 {
-    /// <summary>
-    /// A Variable represents a cell of a sheet-defined function, whether 
-    /// an argument (input cell) or a computed cell (intermediate or output cell).
-    /// </summary>
-    public abstract class Variable : IEquatable<Variable>
+    private readonly string name;
+    private readonly Typ type;
+
+    public Variable(String name, Typ type)
     {
-        private readonly string name;
-        private readonly Typ type;
-
-        public Variable(String name, Typ type)
-        {
-            this.name = name;
-            this.type = type;
-        }
-
-        public bool Equals(Variable that)
-        {
-            return this == that;
-        }
-
-        public override bool Equals(Object obj)
-        {
-            return Equals(obj as Variable);
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
-
-        public abstract void EmitLoad(ILGenerator ilg);
-
-        public abstract void EmitStore(ILGenerator ilg);
-
-        public abstract Variable Fresh();
-
-        public String Name
-        {
-            get { return name; }
-        }
-
-        public Typ Type
-        {
-            get { return type; }
-        }
+        this.name = name;
+        this.type = type;
     }
 
-    /// <summary>
-    /// A LocalVariable represents a .NET IL variable holding a computed
-    /// (intermediate or output) cell of a sheet-defined function.
-    /// </summary>
-    public class LocalVariable : Variable
+    public bool Equals(Variable that)
     {
-        private LocalBuilder localBuilder;  // null until var emitted
-
-        public LocalVariable(String name, Typ type)
-          : base(name, type) { }
-
-        private LocalBuilder GetLocalBuilder(ILGenerator ilg)
-        {
-            if (localBuilder == null)
-            {
-                if (Type == Typ.Number)
-                    this.localBuilder = ilg.DeclareLocal(typeof(double));
-                else
-                    this.localBuilder = ilg.DeclareLocal(Value.type);
-            }
-            return localBuilder;
-        }
-
-        public override void EmitLoad(ILGenerator ilg)
-        {
-            ilg.Emit(OpCodes.Ldloc, GetLocalBuilder(ilg));
-        }
-
-        public override void EmitStore(ILGenerator ilg)
-        {
-            ilg.Emit(OpCodes.Stloc, GetLocalBuilder(ilg));
-        }
-
-        public override Variable Fresh()
-        {
-            return new LocalVariable(Name, Type);
-        }
+        return this == that;
     }
 
-    /// <summary>
-    /// A LocalArgument represents a .NET IL function argument holding
-    /// an input of a sheet-defined function.
-    /// </summary>
-    public class LocalArgument : Variable
+    public override bool Equals(Object obj)
     {
-        private readonly short argumentNumber;
+        return Equals(obj as Variable);
+    }
 
-        public LocalArgument(String name, Typ type, short argumentNumber)
-          : base(name, type)
-        {
-            this.argumentNumber = argumentNumber;
-        }
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
 
-        public override void EmitLoad(ILGenerator ilg)
-        {
-            ilg.Emit(OpCodes.Ldarg, argumentNumber);
-        }
+    public abstract void EmitLoad(ILGenerator ilg);
 
-        public override void EmitStore(ILGenerator ilg)
-        {
-            throw new ImpossibleException("LocalArgument.EmitStore unexpected");
-        }
+    public abstract void EmitStore(ILGenerator ilg);
 
-        public override Variable Fresh()
+    public abstract Variable Fresh();
+
+    public String Name
+    {
+        get { return name; }
+    }
+
+    public Typ Type
+    {
+        get { return type; }
+    }
+}
+
+/// <summary>
+/// A LocalVariable represents a .NET IL variable holding a computed
+/// (intermediate or output) cell of a sheet-defined function.
+/// </summary>
+public class LocalVariable : Variable
+{
+    private LocalBuilder localBuilder;  // null until var emitted
+
+    public LocalVariable(String name, Typ type)
+      : base(name, type) { }
+
+    private LocalBuilder GetLocalBuilder(ILGenerator ilg)
+    {
+        if (localBuilder == null)
         {
-            throw new ImpossibleException("LocalArgument.Fresh()");
+            if (Type == Typ.Number)
+                this.localBuilder = ilg.DeclareLocal(typeof(double));
+            else
+                this.localBuilder = ilg.DeclareLocal(Value.type);
         }
+        return localBuilder;
+    }
+
+    public override void EmitLoad(ILGenerator ilg)
+    {
+        ilg.Emit(OpCodes.Ldloc, GetLocalBuilder(ilg));
+    }
+
+    public override void EmitStore(ILGenerator ilg)
+    {
+        ilg.Emit(OpCodes.Stloc, GetLocalBuilder(ilg));
+    }
+
+    public override Variable Fresh()
+    {
+        return new LocalVariable(Name, Type);
+    }
+}
+
+/// <summary>
+/// A LocalArgument represents a .NET IL function argument holding
+/// an input of a sheet-defined function.
+/// </summary>
+public class LocalArgument : Variable
+{
+    private readonly short argumentNumber;
+
+    public LocalArgument(String name, Typ type, short argumentNumber)
+      : base(name, type)
+    {
+        this.argumentNumber = argumentNumber;
+    }
+
+    public override void EmitLoad(ILGenerator ilg)
+    {
+        ilg.Emit(OpCodes.Ldarg, argumentNumber);
+    }
+
+    public override void EmitStore(ILGenerator ilg)
+    {
+        throw new ImpossibleException("LocalArgument.EmitStore unexpected");
+    }
+
+    public override Variable Fresh()
+    {
+        throw new ImpossibleException("LocalArgument.Fresh()");
     }
 }
--- Workbook.cs
+++ Workbook.cs
@@ -1,4 +1,4 @@
-// Funcalc, a spreadsheet core implementation 
+// Funcalc, a spreadsheet core implementation
 // ----------------------------------------------------------------------
 // Copyright (c) 2006-2014 Peter Sestoft and others
 
@@ -29,228 +29,226 @@ using SC = System.Collections;
 using System.Collections.Generic;
 using Corecalc.Funcalc;
 
-namespace Corecalc
+namespace Corecalc;
+/// <summary>
+/// A Workbook is a collection of distinct named Sheets.
+/// </summary>
+public sealed class Workbook : IEnumerable<Sheet>
 {
-    /// <summary>
-    /// A Workbook is a collection of distinct named Sheets.
-    /// </summary>
-    public sealed class Workbook : IEnumerable<Sheet>
+    public event Action<String[]> OnFunctionsAltered;
+
+    private readonly List<Sheet> sheets            // All non-null and distinct
+      = new List<Sheet>();
+    public readonly Formats format                 // Formula formatting options
+      = new Formats();
+
+    // For managing recalculation of the workbook
+    public CyclicException Cyclic { get; private set; }  // Non-null if workbook has cycle
+    public uint RecalcCount { get; private set; }        // Number of recalculations done
+    public bool UseSupportSets { get; private set; }
+    private readonly List<FullCellAddr> editedCells
+      = new List<FullCellAddr>();
+    private readonly HashSet<FullCellAddr> volatileCells
+      = new HashSet<FullCellAddr>();
+    private readonly Queue<FullCellAddr> awaitsEvaluation
+      = new Queue<FullCellAddr>();
+
+    public Workbook()
     {
-        public event Action<String[]> OnFunctionsAltered;
+        RecalcCount = 0;
+        UseSupportSets = false;
+        SdfManager.ResetTables();
+    }
 
-        private readonly List<Sheet> sheets            // All non-null and distinct
-          = new List<Sheet>();
-        public readonly Formats format                 // Formula formatting options
-          = new Formats();
+    public void AddSheet(Sheet sheet)
+    {
+        sheets.Add(sheet);
+    }
 
-        // For managing recalculation of the workbook
-        public CyclicException Cyclic { get; private set; }  // Non-null if workbook has cycle
-        public uint RecalcCount { get; private set; }        // Number of recalculations done
-        public bool UseSupportSets { get; private set; }
-        private readonly List<FullCellAddr> editedCells
-          = new List<FullCellAddr>();
-        private readonly HashSet<FullCellAddr> volatileCells
-          = new HashSet<FullCellAddr>();
-        private readonly Queue<FullCellAddr> awaitsEvaluation
-          = new Queue<FullCellAddr>();
+    public void RecordCellChange(int col, int row, Sheet sheet)
+    {
+        editedCells.Add(new FullCellAddr(sheet, col, row));
+    }
 
-        public Workbook()
+    public Sheet this[String name]
+    {
+        get
         {
-            RecalcCount = 0;
-            UseSupportSets = false;
-            SdfManager.ResetTables();
+            name = name.ToUpper();
+            foreach (Sheet sheet in sheets)
+                if (sheet.Name.ToUpper() == name)
+                    return sheet;
+            return null;
         }
+    }
 
-        public void AddSheet(Sheet sheet)
-        {
-            sheets.Add(sheet);
-        }
+    public Sheet this[int i]
+    {
+        get { return sheets[i]; }
+    }
 
-        public void RecordCellChange(int col, int row, Sheet sheet)
-        {
-            editedCells.Add(new FullCellAddr(sheet, col, row));
-        }
-
-        public Sheet this[String name]
-        {
-            get
-            {
-                name = name.ToUpper();
-                foreach (Sheet sheet in sheets)
-                    if (sheet.Name.ToUpper() == name)
-                        return sheet;
-                return null;
-            }
-        }
-
-        public Sheet this[int i]
-        {
-            get { return sheets[i]; }
-        }
-
-        // Recalculate from recalculation roots only, using their supported sets
-        public long Recalculate()
-        {
-            // Now Cyclic != null or for all formulas f, f.state==Uptodate
-            if (Cyclic != null || CheckForModifiedSdf())
-                return RecalculateFullAfterSdfCheck();
-            else
-                return TimeRecalculation(delegate
-                {
-                    UseSupportSets = true;
-                    // Requires for all formulas f, f.state==Uptodate
-                    // Stage (1): Mark formulas reachable from roots, f.state=Dirty
-                    SupportArea.IdempotentForeach = true;
-                    foreach (FullCellAddr fca in volatileCells)
-                        Cell.MarkCellDirty(fca.sheet, fca.ca.col, fca.ca.row);
-                    foreach (FullCellAddr fca in editedCells)
-                        Cell.MarkCellDirty(fca.sheet, fca.ca.col, fca.ca.row);
-                    // Stage (2): Evaluate Dirty formulas (and Dirty cells they depend on)
-                    awaitsEvaluation.Clear();
-                    SupportArea.IdempotentForeach = true;
-                    foreach (FullCellAddr fca in editedCells)
-                        Cell.EnqueueCellForEvaluation(fca.sheet, fca.ca.col, fca.ca.row);
-                    foreach (FullCellAddr fca in volatileCells)
-                        Cell.EnqueueCellForEvaluation(fca.sheet, fca.ca.col, fca.ca.row);
-                    while (awaitsEvaluation.Count > 0)
-                        awaitsEvaluation.Dequeue().Eval();
-                });
-        }
-
-        public void AddToQueue(Sheet sheet, int col, int row)
-        {
-            awaitsEvaluation.Enqueue(new FullCellAddr(sheet, col, row));
-        }
-
-        public long RecalculateFull()
-        {
-            CheckForModifiedSdf();
+    // Recalculate from recalculation roots only, using their supported sets
+    public long Recalculate()
+    {
+        // Now Cyclic != null or for all formulas f, f.state==Uptodate
+        if (Cyclic != null || CheckForModifiedSdf())
             return RecalculateFullAfterSdfCheck();
-        }
-
-        // Unconditionally recalculate all cells a la Ctrl+Alt+F9
-        public long RecalculateFullAfterSdfCheck()
-        {
+        else
             return TimeRecalculation(delegate
             {
-                UseSupportSets = false;
-                ResetCellState();
-                // For all formulas f, f.state==Dirty
-                foreach (Sheet sheet in sheets)
-                    sheet.RecalculateFull();
+                UseSupportSets = true;
+                // Requires for all formulas f, f.state==Uptodate
+                // Stage (1): Mark formulas reachable from roots, f.state=Dirty
+                SupportArea.IdempotentForeach = true;
+                foreach (FullCellAddr fca in volatileCells)
+                    Cell.MarkCellDirty(fca.sheet, fca.ca.col, fca.ca.row);
+                foreach (FullCellAddr fca in editedCells)
+                    Cell.MarkCellDirty(fca.sheet, fca.ca.col, fca.ca.row);
+                // Stage (2): Evaluate Dirty formulas (and Dirty cells they depend on)
+                awaitsEvaluation.Clear();
+                SupportArea.IdempotentForeach = true;
+                foreach (FullCellAddr fca in editedCells)
+                    Cell.EnqueueCellForEvaluation(fca.sheet, fca.ca.col, fca.ca.row);
+                foreach (FullCellAddr fca in volatileCells)
+                    Cell.EnqueueCellForEvaluation(fca.sheet, fca.ca.col, fca.ca.row);
+                while (awaitsEvaluation.Count > 0)
+                    awaitsEvaluation.Dequeue().Eval();
             });
-        }
+    }
 
-        public long RecalculateFullRebuild()
-        {
-            return TimeRecalculation(delegate
-            {
-                UseSupportSets = false;
-                RebuildSupportGraph();  // Leaves all cells Dirty
-                foreach (Sheet sheet in sheets)
-                    sheet.RecalculateFull();
-            });
-        }
+    public void AddToQueue(Sheet sheet, int col, int row)
+    {
+        awaitsEvaluation.Enqueue(new FullCellAddr(sheet, col, row));
+    }
 
-        // Timing, and handling of cyclic dependencies
-        private long TimeRecalculation(Action act)
-        {
-            Cyclic = null;
-            RecalcCount++;
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            try
-            {
-                act();
-            }
-            catch (Exception exn)
-            {
-                ResetCellState();       // Mark all cells Dirty
-                if (exn is CyclicException)
-                    Cyclic = exn as CyclicException;
-                else
-                    Console.WriteLine("BAD: {0}", exn);
-            }
-            sw.Stop();
-            editedCells.Clear();
-            return sw.ElapsedMilliseconds;
-        }
+    public long RecalculateFull()
+    {
+        CheckForModifiedSdf();
+        return RecalculateFullAfterSdfCheck();
+    }
 
-        private bool CheckForModifiedSdf()
+    // Unconditionally recalculate all cells a la Ctrl+Alt+F9
+    public long RecalculateFullAfterSdfCheck()
+    {
+        return TimeRecalculation(delegate
         {
-            if (RecalcCount != 0)
-            {
-                String[] modifiedFunctions = SdfManager.CheckForModifications(editedCells);
-                if (modifiedFunctions.Length != 0 && OnFunctionsAltered != null)
-                {
-                    OnFunctionsAltered(modifiedFunctions);
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        private void ResetCellState()
-        {
+            UseSupportSets = false;
+            ResetCellState();
+            // For all formulas f, f.state==Dirty
             foreach (Sheet sheet in sheets)
-                sheet.ResetCellState();
-        }
+                sheet.RecalculateFull();
+        });
+    }
 
-        public void RebuildSupportGraph()
+    public long RecalculateFullRebuild()
+    {
+        return TimeRecalculation(delegate
         {
-            Console.WriteLine("Rebuilding support graph");
-            foreach (Sheet sheet in this)
-                foreach (Cell cell in sheet)
-                    cell.ResetSupportSet();
-            ResetCellState(); // Mark all cells Dirty ie. not Visited
-            foreach (Sheet sheet in this)
-                sheet.AddToSupportSets();
-            // Leaves all cells Dirty
-        }
-
-        public void ResetVolatileSet()
-        {
-            volatileCells.Clear();
-            foreach (Sheet sheet in this)
-                sheet.IncreaseVolatileSet();
-        }
-
-        public void IncreaseVolatileSet(Cell cell, Sheet sheet, int col, int row)
-        {
-            if (cell != null && cell.IsVolatile)
-                volatileCells.Add(new FullCellAddr(sheet, col, row));
-        }
-
-        public void DecreaseVolatileSet(Cell cell, Sheet sheet, int col, int row)
-        {
-            Formula f = cell as Formula;
-            if (f != null)
-                volatileCells.Remove(new FullCellAddr(sheet, col, row));
-        }
-
-        public int SheetCount
-        {
-            get { return sheets.Count; }
-        }
-
-        IEnumerator<Sheet> IEnumerable<Sheet>.GetEnumerator()
-        {
+            UseSupportSets = false;
+            RebuildSupportGraph();  // Leaves all cells Dirty
             foreach (Sheet sheet in sheets)
-                yield return sheet;
-        }
+                sheet.RecalculateFull();
+        });
+    }
 
-        SC.IEnumerator SC.IEnumerable.GetEnumerator()
+    // Timing, and handling of cyclic dependencies
+    private long TimeRecalculation(Action act)
+    {
+        Cyclic = null;
+        RecalcCount++;
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        try
         {
-            foreach (Sheet sheet in sheets)
-                yield return sheet;
+            act();
         }
+        catch (Exception exn)
+        {
+            ResetCellState();       // Mark all cells Dirty
+            if (exn is CyclicException)
+                Cyclic = exn as CyclicException;
+            else
+                Console.WriteLine("BAD: {0}", exn);
+        }
+        sw.Stop();
+        editedCells.Clear();
+        return sw.ElapsedMilliseconds;
+    }
 
-        public void Clear()
+    private bool CheckForModifiedSdf()
+    {
+        if (RecalcCount != 0)
         {
-            sheets.Clear();
-            editedCells.Clear();
-            volatileCells.Clear();
-            awaitsEvaluation.Clear();
+            String[] modifiedFunctions = SdfManager.CheckForModifications(editedCells);
+            if (modifiedFunctions.Length != 0 && OnFunctionsAltered != null)
+            {
+                OnFunctionsAltered(modifiedFunctions);
+                return true;
+            }
         }
+        return false;
+    }
+
+    private void ResetCellState()
+    {
+        foreach (Sheet sheet in sheets)
+            sheet.ResetCellState();
+    }
+
+    public void RebuildSupportGraph()
+    {
+        Console.WriteLine("Rebuilding support graph");
+        foreach (Sheet sheet in this)
+            foreach (Cell cell in sheet)
+                cell.ResetSupportSet();
+        ResetCellState(); // Mark all cells Dirty ie. not Visited
+        foreach (Sheet sheet in this)
+            sheet.AddToSupportSets();
+        // Leaves all cells Dirty
+    }
+
+    public void ResetVolatileSet()
+    {
+        volatileCells.Clear();
+        foreach (Sheet sheet in this)
+            sheet.IncreaseVolatileSet();
+    }
+
+    public void IncreaseVolatileSet(Cell cell, Sheet sheet, int col, int row)
+    {
+        if (cell != null && cell.IsVolatile)
+            volatileCells.Add(new FullCellAddr(sheet, col, row));
+    }
+
+    public void DecreaseVolatileSet(Cell cell, Sheet sheet, int col, int row)
+    {
+        Formula f = cell as Formula;
+        if (f != null)
+            volatileCells.Remove(new FullCellAddr(sheet, col, row));
+    }
+
+    public int SheetCount
+    {
+        get { return sheets.Count; }
+    }
+
+    IEnumerator<Sheet> IEnumerable<Sheet>.GetEnumerator()
+    {
+        foreach (Sheet sheet in sheets)
+            yield return sheet;
+    }
+
+    SC.IEnumerator SC.IEnumerable.GetEnumerator()
+    {
+        foreach (Sheet sheet in sheets)
+            yield return sheet;
+    }
+
+    public void Clear()
+    {
+        sheets.Clear();
+        editedCells.Clear();
+        volatileCells.Clear();
+        awaitsEvaluation.Clear();
     }
 }
